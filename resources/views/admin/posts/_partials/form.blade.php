2a7299eb7879aad102e4e3ba0f3e1e51d0e269ed
Victor Soles <victorsoles09@gmail.com>
2022-03-22 17:17:51 -0300

    Starting DevFinances in laravel

--- resources/views/admin/posts/_partials/form.blade.php
+++ resources/views/admin/posts/_partials/form.blade.php
@@ -7,6 +7,7 @@
 @endif
 
 @csrf
+<input type="file" name="imagem" id="image">
 <input type="text" name="titulo" id="titulo" placeholder="Título" value="{{ $post->titulo ?? old('titulo') }}">
 <input type="text" name="subtitulo" id="subtitulo" placeholder="Subtítulo" value="{{ $post->subtitulo ?? old('subtitulo') }}">
 <textarea name="conteudo" id="conteudo" cols="30" rows="4" placeholder="Texto">{{ $post->conteudo ?? old('conteudo') }}</textarea>
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,7 +6,7 @@
 
 <h1>Cadastrar Novo Post</h1>
 
-<form action="{{ route('posts.store') }}" method="POST">
+<form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
     @include('admin.posts._partials.form')
 </form>
 
